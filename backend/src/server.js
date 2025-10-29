0a3b71de54cd90f3cd863b3c8a42d782e4b1194d
andyw <andywu217@gmail.com>
2025-10-29 22:14:42 +0800

    Fix Express v5 wildcard route error - use middleware instead of app.get('*')

--- backend/src/server.js
+++ backend/src/server.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(frontendPath));
 
   // Serve index.html for all non-API routes (SPA support)
-  app.get('*', (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(frontendPath, 'index.html'));
   });
 } else {
